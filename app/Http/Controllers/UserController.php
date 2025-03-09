eeab289059de3e304cdbbb3742204d643f4a7280
Ananna-ahmd <anuprof29@gmail.com>
2025-03-09 23:50:01 +0600

    User Registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,36 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
+use Exception;
 
 class UserController extends Controller
 {
-    //
+    function UserRegistration(Request $request){
+        try{
+        $email=$request->input('email');
+        $name=$request->input('name');
+        $mobile=$request->input('mobile');
+        $password=$request->input('password');
+
+        User::create([
+            'email'=>$email,
+            'name'=>$name,
+            'mobile'=>$mobile,
+            'password'=>bcrypt($password)
+        ]);
+
+        return response()->json([
+            'status'=>'success',
+            'message'=>'User Registered Successfully'],200);
+    }
+    catch(Exception $e){
+        return response()->json([
+            'status'=>'error',
+            'message'=>$e->getMessage()
+        ],500);
+
+    }
+}
 }
