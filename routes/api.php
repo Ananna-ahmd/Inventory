691d66442b76850aca577927b38dbe66e0cf9107
Ananna-ahmd <anuprof29@gmail.com>
2025-03-14 22:43:34 +0600

    Add OTPMail, reset password

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Middleware\TokenVerificationAPIMiddleWare;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::post('/user-registration', [UserController::class, 'UserRegistration']);
 Route::post('/user-login', [UserController::class, 'UserLogin']);
-Route::get('/logout', [UserController::class, 'UserLogout']);
+Route::post('/logout', [UserController::class, 'UserLogout']);
+Route::get('/send-otp', [UserController::class, 'SendOTPCode']);
+Route::post('/reset-password', [UserController::class, 'ResetPassword'])->middleware([TokenVerificationAPIMiddleWare::class]);
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Middleware\TokenVerificationMiddleWare;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
+route::get('/', function () {
+return view('OTPMail');
+});
 Route::post('/user-registration', [UserController::class, 'UserRegistration']);
 Route::post('/user-login', [UserController::class, 'UserLogin']);
 Route::get('/logout', [UserController::class, 'UserLogout']);
+Route::get('/send-otp', [UserController::class, 'SendOTPCode']);
+Route::post('/reset-password', [UserController::class, 'ResetPassword'])->middleware([TokenVerificationMiddleWare::class]);
